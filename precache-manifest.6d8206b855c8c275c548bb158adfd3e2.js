928c64c8d6c65d9ceee0edf27c1a57e855e72ebe
Mohamed-saied <mohamed00sd6@gmail.com>
2019-02-21 18:18:06 +0200

    Add files via upload

---
+++ precache-manifest.6d8206b855c8c275c548bb158adfd3e2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dcd194ce09eac82110f40376302f7a9",
+    "url": "/static/media/moshem.0dcd194c.jpg"
+  },
+  {
+    "revision": "ad2a2c199d341b784518",
+    "url": "/static/css/main.ba47f83b.chunk.css"
+  },
+  {
+    "revision": "118e0d670cd6da0d44d0fd30b78b49f6",
+    "url": "/static/media/img6.118e0d67.jpg"
+  },
+  {
+    "revision": "35f79652cd9db18dd164",
+    "url": "/static/js/1.35f79652.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89fdf690e0776f3df327189cf2641ba7",
+    "url": "/static/media/mog1.89fdf690.jpg"
+  },
+  {
+    "revision": "c1b83e0eeb7f251f148c0948a6e7b22f",
+    "url": "/static/media/img1.c1b83e0e.jpg"
+  },
+  {
+    "revision": "4cb9d3a509ed2330b37f4f4e1324b8ba",
+    "url": "/static/media/img3.4cb9d3a5.jpg"
+  },
+  {
+    "revision": "db0f3221181957e336847067b5719b95",
+    "url": "/static/media/tech.db0f3221.jpg"
+  },
+  {
+    "revision": "aca8bc21418522bbacb9c9e4ed12022b",
+    "url": "/static/media/mog2.aca8bc21.jpg"
+  },
+  {
+    "revision": "d7b9d003bd3f08f07b12fd7602c72ce0",
+    "url": "/static/media/mog3.d7b9d003.jpg"
+  },
+  {
+    "revision": "7ecda3dcf00a850e730bb275cadea876",
+    "url": "/static/media/img5.7ecda3dc.jpg"
+  },
+  {
+    "revision": "ad2a2c199d341b784518",
+    "url": "/static/js/main.ad2a2c19.chunk.js"
+  },
+  {
+    "revision": "d7d3cfeb65422cab41b3daaa0e39961d",
+    "url": "/static/media/logo.d7d3cfeb.png"
+  },
+  {
+    "revision": "328878bf84a9c0f819158286aee7d092",
+    "url": "/static/media/img2.328878bf.jpg"
+  },
+  {
+    "revision": "f5aad042fb7a1ff617d124913da082c0",
+    "url": "/static/media/cement.f5aad042.png"
+  },
+  {
+    "revision": "a822e74bf43d2526ef48d777518353a1",
+    "url": "/static/media/crane.a822e74b.png"
+  },
+  {
+    "revision": "fbc45f90b1dc6985b32cb07171f63848",
+    "url": "/static/media/labor-man.fbc45f90.png"
+  },
+  {
+    "revision": "639d6e8368b7bee5cfcd94e4fe40535a",
+    "url": "/static/media/project.639d6e83.jpg"
+  },
+  {
+    "revision": "bb9fd29d19b36110b7838cf91c5c77e6",
+    "url": "/static/media/naeb.bb9fd29d.jpg"
+  },
+  {
+    "revision": "304ce940b0aece4c88afbb6d455ada7b",
+    "url": "/static/media/money.304ce940.jpg"
+  },
+  {
+    "revision": "4a15d49e5716e1620ec26dc8a6e29f93",
+    "url": "/static/media/edara.4a15d49e.gif"
+  },
+  {
+    "revision": "e01902c1c47bc84b9a62078c0517c563",
+    "url": "/static/media/aqar.e01902c1.jpg"
+  },
+  {
+    "revision": "35f79652cd9db18dd164",
+    "url": "/static/css/1.39dd8da8.chunk.css"
+  },
+  {
+    "revision": "195cb7e5c076494f1d82f0ce322df9bc",
+    "url": "/index.html"
+  }
+];
